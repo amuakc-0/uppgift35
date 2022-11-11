deae00149c40d79c65ab913c1ff0d4e9e3ea5c2e
amuakc-0 <amuakc-0@student.ltu.se>
2022-11-11 11:46:25 +0100

    reg_ResultatTest i LadokController påbörjad för att kunna ta emot form data istället för parametrar

--- src/main/java/dev/uppgift3/control/LadokController.java
+++ src/main/java/dev/uppgift3/control/LadokController.java
@@ -5,8 +5,6 @@ import dev.uppgift3.model.LadokResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +18,21 @@ public class LadokController {
 
     //TODO: Place post, put, get, delete mapping here
 
+
+    //UNDER CONSTRUCTION
+    @RequestMapping("/reg_ResultatTest")
+    public String regResultatTest(@RequestParam(value = "studentData[]") String[] studentData) {
+        //List of all students used for returning a list of status for all students for the course
+        List<String> studentList = new ArrayList<>();
+        //Loops through array studentData and finds the pattern in database
+        //(int i = 0; i < cars.length; i++)
+        for (int i = 0; i < studentData.length; i++) {
+            studentList.add(studentData[i]);
+            System.out.println(studentList);
+        }
+        return "klar";
+    }
+
     //End point for reg_Resultat. Posts data needed for creating a new LadokResult object. TODO: look into if other datatypes are better
     @PostMapping("/reg_Resultat")
     public List<LadokResult> regResultat(@RequestParam String pnr, @RequestParam String kurskod, @RequestParam String modul,
@@ -28,7 +41,7 @@ public class LadokController {
         //List of all students used for returning a list of status for all students for the course
         List<LadokResult> studentList = new ArrayList<LadokResult>();
 
-        //TODO: change to @requestBody might be necessary to handle several students at the same time then loop the following:
+        //TODO: change to @requestBody might be necessary to handle several students at the same time then loop the following (har börjat med reg_ResultatTest ovan):
 
         //Calls reg_Result method from implementation class to each save result to DB, get LadokResult object back
        LadokResult newResult = ladokServiceImplementation.reg_Result(pnr, kurskod, modul, betyg);
